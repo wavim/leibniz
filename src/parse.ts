fa3244c78627315cd6d3d50b6b8276219b6d79d0
wavim <wavim.wu@gmail.com>
2025-09-01 18:17:49 +0800

    feat: lexer implementation

---
+++ src/parse.ts
@@ -0,0 +1,28 @@
+import { Token } from "./types/token";
+
+const lexime = {
+	lbracket: "(",
+	rbracket: ")",
+	negation: "!",
+	conjunct: "&",
+	disjunct: "|",
+} as const satisfies Record<Exclude<Token["kind"], "variable">, string>;
+
+const regexp = RegExp(
+	Object.entries(lexime).map(([kind, raw]) => `(?<${kind}>\\${raw})`).join("|")
+		+ "|"
+		+ `(?<variable>[^ ${Object.values(lexime).join("")}]+)`,
+	"g",
+);
+
+function lexes(expression: string): Token[] {
+	return [...expression.matchAll(regexp)].map((match) => {
+		const selectors = match.groups as Record<Token["kind"], string | undefined>;
+
+		return {
+			kind: Object.keys(selectors).find((k) => selectors[k]) as Token["kind"],
+			raw: match[0],
+			pos: match.index,
+		};
+	});
+}
